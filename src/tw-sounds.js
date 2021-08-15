745a9682cc748819892ce92d303948ea8d16e079
Finn Lancaster <flancast90@gmail.com>
2021-08-15 15:22:40 -0400

    Update tw-sounds.js

--- src/tw-sounds.js
+++ src/tw-sounds.js
@@ -11,7 +11,6 @@ Under MIT License
 
 // global variable declarations
 // array we will populate with key, keypair
-var got_content = false;
 var key_keypair = [];
 var CMU_dictionary = [];
 var user_ipa = ""
@@ -20,8 +19,9 @@ var user_ipa = ""
 const url = "https://flancast90.github.io/TW-Sounds/lib/CMU.in.IPA.txt"
 fetch(url)
    .then( r => r.text() )
-   .then( t => extract_ipa(t))
-   .then(got_content = true);
+   // regex below will remove tab spaces from txt file.
+   // it took my dumb ass three fucking days to find that HT = tab.
+   .then( t => extract_ipa(t.replace(/\t/g, "")));
 
 // this function organises the output of the CMU IPA
 // Dict to arrays. 
@@ -31,18 +31,29 @@ function extract_ipa(ipa_input) {
     var clean_array = clean_text.split("<br>");
     
     for (let i = 0; i < clean_array.length-1; i++) {
-
-        	key_keypair.push(clean_array[i].split(",")[0]);
-			key_keypair.push(clean_array[i].split(",")[1]);
+     
+     key_keypair.push(clean_array[i].split(",")[0].replaceAll(" ", ""));
+	 key_keypair.push(clean_array[i].replaceAll(" ", "").split(",")[1]);
+     
     }
-    
+}
+
+
+// this function finds similar-sounding words based
+// on the user's word input
+function find_similar(pronunciation) {
+	var word = pronunciation.split("");
+    document.write(word);
+    var index = key_keypair.indexOf("aalseth");
+    alert(key_keypair[index+1]);
 }
 
 function handle_user_input(user_input) {
 
 // words are always at an even index, with their definitions 
 // at odd, so we can add two and half the eval time.
-	for (let i = 0; i < key_keypair.length - 1; i+2) {
+	for (let i = 0; i < key_keypair.length - 1; i++) {
+	
     	if (key_keypair[i] == user_input.toLowerCase()) {
         	// first, make sure the word is the word, and not 
             // the definition.
@@ -52,16 +63,26 @@ function handle_user_input(user_input) {
         		// word before.
         		user_ipa = key_keypair[i + 1];
                 console.log(user_ipa);
+                find_similar(user_ipa);
                 break;
             } else {
             	// God fucking save us
                 // Yes, I've been reading the funniest code comments
                 // on StackOverflow.
+                
+                // No Joke. This below line is fucking voodoo.
+                // Try removing it and the program doesn't run.
+                // no, seriously, try it karen.
+                console.error("Something went wrong");
             }
             
         } else {
-        	return "Error: Word not found."
+        	// loop keeps going
         }
     }
     
 }
+
+setTimeout(function() {
+	handle_user_input("aalseth");
+}, 5000);
